d7c9b27d648b9c951d0cb676fe256f204b67b5d3
AndDiSa <anddisa@googlemail.com>
2026-06-08 14:52:25 +0200

    fix mergig issues

--- utils/src/bt_utils.c
+++ utils/src/bt_utils.c
@@ -182,7 +182,6 @@ void adjust_priority_a2dp(int start) {
         }
     }
 }
-}
 
 /*****************************************************************************
 **
@@ -198,3 +197,4 @@ const char* addr_to_string(const uint8_t addr[6]) {
     snprintf(str, sizeof(str), "%02x:%02x:%02x:%02x:%02x:%02x",
              addr[0], addr[1], addr[2], addr[3], addr[4], addr[5]);
     return str;
+}
